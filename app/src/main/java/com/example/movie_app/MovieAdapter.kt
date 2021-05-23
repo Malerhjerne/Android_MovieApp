304e38868d5ec7faaa0cceeb877a5e374a4b7d21
Malerhjerne <55696404+Malerhjerne@users.noreply.github.com>
2021-05-23 12:27:43 +0200

    Base Setup

--- app/src/main/java/com/example/movie_app/MovieAdapter.kt
+++ app/src/main/java/com/example/movie_app/MovieAdapter.kt
@@ -34,7 +34,7 @@ class MovieAdapter(private val clickListener:OnItemClickListener) : RecyclerView
             imageView = view.findViewById(R.id.movieImageView)
             view.setOnClickListener(this)
         }
-        override fun onClick(v: Viev?) {
+        override fun onClick(v: VieW?) {
             val position = adapterPosition
             if(position != RecyclerView.NO_POSITION) {
                 clickListener.onItemClick(position);
